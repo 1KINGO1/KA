024d4b7e831d96b2241a29e687e47c3715a8a8d3
KINGO <spental12345@gmail.com>
2024-03-20 19:03:24 +0200

    Added reading lines from test.in & outputting them

--- src/main.asm
+++ src/main.asm
@@ -11,7 +11,7 @@ main PROC
     mov cl, ds:[80h]   ; at offset 80h length of "args"
     write_char:
         test cl, cl
-        jz write_end
+        jz read_next
         mov si, 81h        ; at offest 81h first char of "args"
         add si, cx
         mov ah, 02h
@@ -19,6 +19,25 @@ main PROC
         int 21h
         dec cl
         jmp write_char
-    write_end:
+
+    read_next:
+        mov ah, 3Fh       ; Функція 3Fh - зчитування з файлу або пристрою
+        mov bx, 0         ; stdin - дескриптор вводу (стандартний ввід)
+        mov cx, 1         ; Читати 1 байт
+        mov dx, offset oneChar ; Покажчик на змінну для зберігання одного символу
+        int 21h           ; Виклик системного переривання для читання
+        ; Перевірка чи досягнуто кінця файлу (EOF)
+        or ax, ax         ; Перевірка на те, чи рівне ax нулю (кінець файлу)
+        jz read_end    ; Якщо ax = 0, це означає, що був досягнутий кінець файлу
+
+        ; Вивід символу, який було прочитано
+        mov ah, 02h       ; Функція 02h - виведення символу
+        mov dl, [oneChar] ; Завантаження символу для виводу
+        int 21h           ; Виклик системного переривання для виводу
+        ; Повторення циклу читання
+        jmp read_next
+    read_end:
+        mov ah, 4Ch       ; Функція 4Ch - вихід з програми
+        int 21h
 main ENDP
 END main
